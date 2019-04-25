53705123c32ce7ab97218e92ec032c9e6119f2a5
cbonnell28 <cbonnell28@gmail.com>
2019-04-25 19:00:53 +0100

    Modified kv file to use a kv file and a py file instead of just py

--- Lampi/lampi/beacon_app.py
+++ Lampi/lampi/beacon_app.py
@@ -1,92 +1,17 @@
 #!/usr/bin/env python
+import kivy
+import json
+import pigpio
+import paho.mqtt.client as mqtt
+import shelve
+import colorsys
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
-Builder.load_string("""
-<HomeScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Button:
-            text: 'Character'
-            on_press: root.manager.current = 'character'
-        Button:
-            text: 'Shop'
-            on_press: root.manager.current = 'shop'
-        Button:
-            text: 'Search'
-            on_press: root.manager.current = 'search'
-
-<CharacterScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Bilbo Baggins'
-        Label:
-            text: 'Attack: 10'
-        Label:
-            text: 'HP: 100'
-        Button:
-            text: 'Home'
-            on_press: root.manager.current = 'home'
-
-<ShopScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Weapon Shop'
-        BoxLayout:
-            BoxLayout:
-                orientation: 'vertical'
-                Label:
-                    text: 'Greatsword'
-                Label:
-                    text: '100 gold'
-            Button:
-                text: 'Buy'
-        BoxLayout:
-            BoxLayout:
-                orientation: 'vertical'
-                Label:
-                    text: 'Fire Staff'
-                Label:
-                    text: '50 gold'
-            Button:
-                text: 'Buy'
-        BoxLayout:
-            BoxLayout:
-                orientation: 'vertical'
-                Label:
-                    text: 'Steel Armor'
-                Label:
-                    text: '200 gold'
-            Button:
-                text: 'Buy'
-        Button:
-            text: 'Home'
-            on_press: root.manager.current = 'home'
-
-
-<SearchScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Nearby Beacons'
-        BoxLayout:
-            Label:
-                text: 'Davids Beacon'
-            Button:
-                text: 'Battle!'
-        BoxLayout:
-            Label:
-                text: 'Christians Beacon'
-            Button:
-                text: 'Battle!'
-        Button:
-            text: 'Home'
-            on_press: root.manager.current = 'home'
-
-""")
+Builder.load_file("beacon.kv")
 
 class HomeScreen(Screen):
     pass
@@ -101,10 +26,10 @@ class SearchScreen(Screen):
     pass
 
 sm = ScreenManager()
-sm.add_widget(HomeScreen(name='home'))
-sm.add_widget(CharacterScreen(name='character'))
-sm.add_widget(ShopScreen(name='shop'))
-sm.add_widget(SearchScreen(name='search'))
+sm.add_widget(HomeScreen(name="home"))
+sm.add_widget(CharacterScreen(name="character"))
+sm.add_widget(ShopScreen(name="shop"))
+sm.add_widget(SearchScreen(name="search"))
 
 class TestApp(App):
 
